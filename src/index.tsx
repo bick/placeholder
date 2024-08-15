c2b62d5f5522c16797a58187d97a916d6c8bd11c
Owen Bick <12536625+bick@users.noreply.github.com>
2024-08-15 16:08:22 -0500

    Add default font

--- src/index.tsx
+++ src/index.tsx
@@ -20,7 +20,7 @@ const Placeholder: React.FC<PlaceholderProps> = ({
     const svgContent = `
     <svg width="${width}" height="${height}" xmlns="http://www.w3.org/2000/svg">
       <rect width="100%" height="100%" fill="${background}" />
-      <text x="50%" y="50%" dominant-baseline="middle" text-anchor="middle" fill="${color}" font-size="20">
+      <text x="50%" y="50%" dominant-baseline="middle" font-family="-apple-system, Inter, sans-serif" text-anchor="middle" fill="${color}" font-size="20">
         ${displayText}
       </text>
     </svg>
